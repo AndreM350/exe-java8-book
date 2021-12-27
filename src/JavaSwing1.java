2248ef1fdacbfeb360f5d585cbe41634691e907d
AndreCPS <andrecmoura2@gmail.com>
2021-12-27 09:59:28 -0300

    commit1

---
+++ src/JavaSwing1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class JavaSwing1 {
+
+    public static void main(String[] args) {
+
+        String nome = JOptionPane.showInputDialog("Insira seu nome");
+        JOptionPane.showMessageDialog(null, nome);
+
+        
+    }
+
+}
